f8b9344e94f771238b087b95c27da9c431e69eb5
young <343284587@qq.com>
2019-05-09 17:57:51 +0800

    面包屑 商品列表 热销列表

---
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -0,0 +1,87 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django import http
+from django.views import View
+
+from goods.models import GoodsCategory
+from contents.utils import get_categories
+from .utils import get_breadcrumb
+from meiduo_mall.utils.response_code import RETCODE
+class ListView(View):
+    '''商品列表界面'''
+    def get(self, request, category_id, page_num):
+
+        """
+            :param category_id: 当前选择的三级类别id
+            :param page_num: 第几页
+            """
+        try:
+            category = GoodsCategory.objects.get(id=category_id)
+        except GoodsCategory.DoesNotExist:
+            return http.HttpResponseForbidden('商品ID不存在')
+        # 获取查询参数中的sort 排序规则
+        sort = request.GET.get('sort', 'default')
+        if sort == 'price':
+            sort_fields = 'price'
+        elif sort == '-sales':
+            sort_fields = '-sales'
+        else:
+            sort_fields = 'create_time'
+
+        # 面包屑导航数据
+        # a = (page_num - 1) * 5
+        # b = a + 5
+        # 查询当前三级类别下面的所有sku
+        # order_by(只能放当前查询集中每个模型中的字段)
+        sku_qs = category.sku_set.filter(is_launched=True).order_by(sort_fields)
+        # 创建分页对象, 每页5条数据
+        # Paginator(要进行分页的所有数据, 每页显示多少条数据)
+        paginator = Paginator(sku_qs, 5)
+
+        # 获取指定界面的sku数据
+        page_skus = paginator.page(page_num)
+        # 获取当前的总页数
+        total_page = paginator.num_pages
+
+        # 页面渲染
+
+        context = {
+
+            'categories': get_categories(),# 频道分类
+            'breadcrumb': get_breadcrumb(category),# 面包屑导航
+            'sort': sort,# 排序字段
+            'category': category,# 第三级分类
+            'page_skus': page_skus,# 分页后数据
+            'total_page': total_page,# 总页数
+            'page_num': page_num# 当前页码
+
+        }
+
+        return render(request, 'list.html', context)
+
+
+class HotGoodsView(View):
+    '''热销排行'''
+    def get(self, request, category_id):
+
+        try:
+            category = GoodsCategory.objects.get(id=category_id)
+        except GoodsCategory.DoesNotExist:
+            return http.HttpResponseForbidden('商品不存在')
+
+        # 获取当前三级类别下面销量最高的前两个sku
+        sku_qs = category.sku_set.filter(is_launched=True).order_by('-sales')[0:2]
+        # 包装两个热销商品字典
+        hot_skus = []
+        # 渲染商品数据
+        for sku in sku_qs:
+            hot_skus.append({
+
+                'id': sku.id,
+                'name': sku.name,
+                'price': sku.price,
+                'default_image_url': sku.default_image.url
+
+            })
+
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'hot_skus': hot_skus})
